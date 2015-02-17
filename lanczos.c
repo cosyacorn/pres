6069d96890e22517fa6186e2b7566aa7d285e413
Shane Harding <sharding@tcd.ie>
2015-02-17 10:13:23 +0000

    messing about

--- lanczos.c
+++ lanczos.c
@@ -1,24 +1,47 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int ** matmul(int **A, int **B, int size){
+
+int** Mat(int n, int m)
+{
+  int r, i, j;
+  int ** mat = (int **)malloc(sizeof(int *) * n);
+
+  for(i=0;i<n;i++){
+    mat[i] = (int *)malloc(sizeof(int)*m);
+      for(j=0; j<m; j++){
+	r=rand()%10;
+	mat[i][j]=r-2;
+      }
+  }
+
+  return mat;
+  for(i=0;i<n;i++) free(mat[i]);
+  free(mat);
+}
+
+int ** matmul(int **A, int **B, int nrow, int ncol, int ncol2){
 
   int i, j, k;
   int **C;
 
-  C=(int**)malloc(sizeof(int*)*size);
+  C=(int**)malloc(sizeof(int*)*nrow);
 
-  for(i=0;i<size;i++){
-    c[i]= (int*)malloc(sizeof(int)*size);
-     for(j=0;j<size;j++){
-        for(k=0;k<size;k++){
+  for(i=0;i<nrow;i++){
+    C[i]= (int*)malloc(sizeof(int)*ncol2);
+     for(j=0;j<ncol2;j++){
+        for(k=0;k<ncol;k++){
 	  C[i][j]+=A[i][k]*B[k][j];
 	}
      }
   }
   return C;
-  for(i=0;i<size;i++){
+  for(i=0;i<nrow;i++){
     free(C[i]);
   }
   free(C);
+}
+
+int main(){
+
 }
